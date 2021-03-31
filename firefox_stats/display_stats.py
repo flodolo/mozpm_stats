6e5fa5684563fb1ac9aea6b3883c5ef2a8548e3a
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2021-03-31 19:38:00 +0200

    Add monthly data to output

--- firefox_stats/display_stats.py
+++ firefox_stats/display_stats.py
@@ -18,24 +18,66 @@ def main():
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
 
+    month_names = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ]
+
     for year in years:
-        # Get totals
+
+        # Get totals per month
+        month_totals = {}
+        for month in range(1, 13):
+            start_date = f"{year}{month:02}01"
+            if month == 12:
+                end_date = f"{year+1}0101"
+            else:
+                end_date = f"{year}{month+1:02}01"
+
+            cursor.execute(
+                "SELECT \
+                SUM(total_added) as added, \
+                SUM(total_added_w) as added_w, \
+                SUM(browser_added) as browser, \
+                SUM(browser_added_w) as browser_w, \
+                SUM(devtools_added) as devtools, \
+                SUM(devtools_added_w) as devtools_w, \
+                SUM(shared_added) as shared, \
+                SUM(shared_added_w) as shared_w, \
+                SUM(total_removed) as removed, \
+                SUM(total_removed_w) as removed_w \
+                FROM stats WHERE day>=? AND day<?",
+                (f"{start_date}", f"{end_date}"),
+            )
+            month_totals[month] = cursor.fetchone()
+
+        # Get totals per year
         cursor.execute(
             "SELECT \
-             SUM(total_added) as added, \
-             SUM(total_added_w) as added_w, \
-             SUM(browser_added) as browser, \
-             SUM(browser_added_w) as browser_w, \
-             SUM(devtools_added) as devtools, \
-             SUM(devtools_added_w) as devtools_w, \
-             SUM(shared_added) as shared, \
-             SUM(shared_added_w) as shared_w, \
-             SUM(total_removed) as removed, \
-             SUM(total_removed_w) as removed_w \
-             FROM stats WHERE day>=? AND day<=?",
+            SUM(total_added) as added, \
+            SUM(total_added_w) as added_w, \
+            SUM(browser_added) as browser, \
+            SUM(browser_added_w) as browser_w, \
+            SUM(devtools_added) as devtools, \
+            SUM(devtools_added_w) as devtools_w, \
+            SUM(shared_added) as shared, \
+            SUM(shared_added_w) as shared_w, \
+            SUM(total_removed) as removed, \
+            SUM(total_removed_w) as removed_w \
+            FROM stats WHERE day>=? AND day<=?",
             (f"{year}0101", f"{year}1231"),
         )
-        totals = cursor.fetchone()
+        year_totals = cursor.fetchone()
 
         # Get last total of the year
         cursor.execute(
@@ -46,21 +88,34 @@ def main():
 
         print(f"{year}:")
         print(
-            f"  Total: {final_total['total']} ({final_total['total_w']}) - Added: {totals['added']} ({totals['added_w']}) - Removed: {totals['removed']} ({totals['removed_w']})"
+            f"  Total: {final_total['total']} ({final_total['total_w']}) - Added: {year_totals['added']} ({year_totals['added_w']}) - Removed: {year_totals['removed']} ({year_totals['removed_w']})"
         )
         print("  Added breakdown:")
-        browser_perc = round(float(totals["browser"]) / totals["added"] * 100, 2)
-        devtools_perc = round(float(totals["devtools"]) / totals["added"] * 100, 2)
-        shared_perc = round(float(totals["shared"]) / totals["added"] * 100, 2)
-        print(
-            f"    Browser: {totals['browser']} ({totals['browser_w']}) - {browser_perc} %"
+        browser_perc = round(
+            float(year_totals["browser"]) / year_totals["added"] * 100, 2
+        )
+        devtools_perc = round(
+            float(year_totals["devtools"]) / year_totals["added"] * 100, 2
+        )
+        shared_perc = round(
+            float(year_totals["shared"]) / year_totals["added"] * 100, 2
         )
         print(
-            f"    DevTools: {totals['devtools']} ({totals['devtools_w']}) - {devtools_perc} %"
+            f"    Browser: {year_totals['browser']} ({year_totals['browser_w']}) - {browser_perc} %"
         )
         print(
-            f"    Shared: {totals['shared']} ({totals['shared_w']}) - {shared_perc} %"
+            f"    DevTools: {year_totals['devtools']} ({year_totals['devtools_w']}) - {devtools_perc} %"
         )
+        print(
+            f"    Shared: {year_totals['shared']} ({year_totals['shared_w']}) - {shared_perc} %"
+        )
+
+        print("  Month breakdown (added):")
+        for month in range(1, 13):
+            if month in month_totals and month_totals[month]["added"] is not None:
+                print(
+                    f"    {month_names[month-1]}: {month_totals[month]['added']} ({month_totals[month]['added_w']})"
+                )
 
     # Clean up and close connection
     connection.close()
